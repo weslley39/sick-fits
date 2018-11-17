ce35cfb7ace1d4cf1d238ab763c67e09d41765dd
Weslley Neri <weslley39@gmail.com>
2018-11-16 23:09:10 -0200

    listing and creating new items to sell

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,19 @@
-const mutations = {};
+const mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO Check if user is logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+
+    console.log(item);
+
+    return item;
+  }
+};
 
 module.exports = mutations;
