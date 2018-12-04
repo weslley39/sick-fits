56de4b85e99fa7b033a05328db07fff030e427ee
Weslley Neri <weslley39@gmail.com>
2018-12-04 17:53:01 -0200

    nav

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,6 +1,9 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+const MAX_AGE = 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+
+
 const mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO Check if user is logged in
@@ -50,7 +53,25 @@ const mutations = {
 
     ctx.response.cookie('token', token, {
       httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+      maxAge: MAX_AGE
+    });
+
+    return user;
+  },
+  async signin(parent, { email, password }, ctx, info) {
+    const user = await ctx.db.query.user({ where: { email }});
+    if (!user) {
+      throw new Error(`No such user found for email -> ${email}`);
+    }
+
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new Error (`Invalid Paasword!`);
+    }
+
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: MAX_AGE,
     });
 
     return user;
