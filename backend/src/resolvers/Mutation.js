16941dd7cd33c9e1786e7b830d323b065d3e08fc
Weslley Neri <weslley39@gmail.com>
2018-12-06 23:04:14 -0200

    update permissions

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const { randomBytes } = require('crypto');
 const { promisify } = require('util');
 const { transport, makeANiceEmail } = require('../mail');
+const { hasPermission } = require('../utils');
 
 const MAX_AGE = 1000 * 60 * 60 * 24 * 365; // 1 year cookie
 
@@ -144,6 +145,28 @@ const mutations = {
       maxAge: MAX_AGE,
     });
     return updatedUser;
+  },
+  async updatePermissions(parent, args, ctx, info) {
+    if (!ctx.request.userId) throw new Error('You must be logged in');
+
+    const currentUser = await ctx.db.query.user({
+      where: {
+        id: ctx.request.userId,
+      }
+    }, info);
+
+    hasPermission(currentUser, ['ADMIN', 'PERMISSIONUPDATE']);
+
+    return ctx.db.mutation.updateUser({
+      data: {
+        permissions: {
+          set: args.permissions
+        },
+      },
+      where: {
+        id: args.userId,
+      }
+    }, info);
   }
 };
 
