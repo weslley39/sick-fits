04aadc7111f7bb4cbe5bf5740e90fba783cbb628
Weslley Neri <weslley39@gmail.com>
2018-12-19 17:52:15 -0200

    add to cart

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -176,6 +176,44 @@ const mutations = {
         id: args.userId,
       }
     }, info);
+  },
+
+  async addToCart(parent, args, ctx, info) {
+    const { userId } = ctx.request;
+
+    // 1. Check if the user is signed in
+    if (!userId) {
+      throw new Error('You must be signed in sooon!');
+    }
+
+    // 2. Query the users current cart
+    const [existingCartItem] = await ctx.db.query.cartItems({
+      where: {
+        user: { id: userId },
+        item: { id: args.id },
+      },
+    });
+
+    // 3. Check if that item is already in their cart and increment by 1
+    if (existingCartItem) {
+      console.log('This item is already in their cart');
+      return ctx.db.mutation.updateCartItem({
+        where: { id: existingCartItem.id },
+        data: { quantity: existingCartItem.quantity + 1 },
+      }, info);
+    }
+
+    // 4. If its not, create a fresh new CartItem
+    return ctx.db.mutation.createCartItem({
+      data: {
+        user: {
+          connect: { id: user },
+        },
+        item: {
+          connect: { id: args.id },
+        },
+      },
+    }, info);
   }
 };
 
