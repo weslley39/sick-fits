93a5bf8ed5238435b0be4f56a794f607bb1890ce
Weslley Neri <weslley39@gmail.com>
2018-12-19 15:32:59 -0200

    cart

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,7 +1,11 @@
+import { Mutation } from 'react-apollo';
+
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
 import User from './User';
 import Signout from './Signout';
+
+import { TOGGLE_CART_MUTATION } from './Cart';
 
 
 const Nav = () => (
@@ -23,6 +27,13 @@ const Nav = () => (
               <a>Account</a>
             </Link>
             <Signout />
+            <Mutation mutation={TOGGLE_CART_MUTATION}>
+              {
+                (toggleCart) => (
+                  <button onClick={toggleCart}>My Cart</button>
+                )
+              }
+            </Mutation>
           </>
         )}
         {!me && (
